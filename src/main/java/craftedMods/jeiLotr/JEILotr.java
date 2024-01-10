a66b1ae8a5568c31d40dddd38e5ec52ac17bc2e4
maximuslotro <61996354+maximuslotro@users.noreply.github.com>
2024-01-09 20:17:32 -0600

    Fix license file build error

--- src/main/java/craftedMods/jeiLotr/JEILotr.java
+++ src/main/java/craftedMods/jeiLotr/JEILotr.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020-2022 CraftedMods (see http://github.com/CraftedMods)
+ * Copyright (C) 2020-2024 CraftedMods (see http://github.com/CraftedMods)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
